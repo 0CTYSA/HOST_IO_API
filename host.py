e909dc025b729149350c2a37835ac8890a1b41c8
XATCSN <119985966+0CTYSA@users.noreply.github.com>
2024-04-21 11:51:01 -0500

    Up code

--- host.py
+++ host.py
@@ -9,9 +9,15 @@ headers = {'Authorization': f'Bearer {api_token}'}
 # Dominio o IP a analizar
 domain = 'ejemplo.com'
 
-# Crea la carpeta para los resultados si no existe
-if not os.path.exists('resultados'):
-    os.makedirs('resultados')
+# Crear la carpeta principal para los resultados si no existe
+base_path = 'resultados'
+if not os.path.exists(base_path):
+    os.makedirs(base_path)
+
+# Crear una subcarpeta para el dominio específico
+domain_path = os.path.join(base_path, domain)
+if not os.path.exists(domain_path):
+    os.makedirs(domain_path)
 
 # Función para realizar las consultas a la API y guardar los resultados
 
@@ -20,11 +26,11 @@ def fetch_and_save_data(endpoint, filename):
     url = f'https://host.io/api/{endpoint}/{domain}?token={api_token}'
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        with open(f'resultados/{filename}.json', 'w') as file:
+        with open(os.path.join(domain_path, f'{filename}.json'), 'w') as file:
             json.dump(response.json(), file, indent=4)
     else:
-        print(f'Error: {response.json()[
-              "error"]} al intentar obtener datos de {endpoint}')
+        print(f'Error: {response.json().get(
+            "error", "Unknown error")} al intentar obtener datos de {endpoint}')
 
 
 # Obtener y guardar datos
@@ -32,4 +38,4 @@ fetch_and_save_data('web', 'web_info')
 fetch_and_save_data('dns', 'dns_info')
 fetch_and_save_data('related', 'related_info')
 
-print("Datos guardados en la carpeta 'resultados'")
+print(f"Datos guardados en la carpeta '{domain_path}'")
